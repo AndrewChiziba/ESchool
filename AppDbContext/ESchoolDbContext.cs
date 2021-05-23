5ef2f8601d46430c1e1bd8ef4ee2a351f85a8dbd
AndrewChiziba <chizibaandrew@gmail.com>
2021-05-24 00:46:26 +0300

    Add project files.

---
+++ AppDbContext/ESchoolDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ESchool.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESchool.AppDbContext
+{
+    public class ESchoolDbContext : IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public ESchoolDbContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+        //DbSets below
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESchool.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Фамилия")]
+        public string Surname { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Отчество")]
+        public string MiddleName { get; set; }
+
+        [NotMapped]
+        public bool IsTeacher { get; set; }
+    }
+}
