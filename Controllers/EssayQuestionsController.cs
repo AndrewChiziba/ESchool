f60953f84bebd5c595ef202e1579ba7ecbdc4869
AndrewChiziba <chizibaandrew@gmail.com>
2021-06-16 15:30:39 +0300

    mid app testing

--- Controllers/EssayQuestionsController.cs
+++ Controllers/EssayQuestionsController.cs
@@ -89,8 +89,21 @@ namespace ESchool.Controllers
             return View();
         }
 
+        //View Essay
+        public IActionResult ViewEssay(string filePath)
+        {
+
+            //byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            //string fileName = "myfile.pdf";
+            // return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            //For preview pdf and the download it use below code
+            var stream = new FileStream(filePath, FileMode.Open);
+            return new FileStreamResult(stream, "application/pdf");
+        }
+
         //download Essay
-        public IActionResult DownloadFile(string filePath)
+        public IActionResult DownloadEssay(string filePath)
         {
 
             byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
--- Controllers/PdfMaterialsController.cs
+++ Controllers/PdfMaterialsController.cs
@@ -70,8 +70,21 @@ namespace ESchool.Controllers
             return View();
         }
 
+        //View file
+        public IActionResult ViewFile(string filePath)
+        {
+
+            //byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            //string fileName = "myfile.pdf";
+           // return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            //For preview pdf and the download it use below code
+             var stream = new FileStream(filePath, FileMode.Open);
+            return new FileStreamResult(stream, "application/pdf");
+        }
+
         //download file
-        public IActionResult DownloadEssay(string filePath)
+        public IActionResult DownloadFile(string filePath)
         {
 
             byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
@@ -79,7 +92,7 @@ namespace ESchool.Controllers
             return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
 
             //For preview pdf and the download it use below code
-             //var stream = new FileStream(filePath, FileMode.Open);
+            //var stream = new FileStream(filePath, FileMode.Open);
             //return new FileStreamResult(stream, "application/pdf");
         }
     }
